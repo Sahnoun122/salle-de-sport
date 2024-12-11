4a29604062a7fcea7fe64e75e8d1bee8a908b96c
khadiijaa123 <Khadijasahnoun70@gmail.com>
2024-12-11 22:36:59 +0100

    ajouter le formailaire d'ajout

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 
 <?php
 
-
+//update
 
   require 'connection.php';
   if(isset($_GET['id'])){
@@ -26,9 +26,6 @@
     $email=$rows['email'];
     $phone=$rows['phone'];
   }
-
-
-
 
 ?>
 
@@ -71,7 +68,8 @@
   </div>
 </nav>
 
-<form class="max-w-md mx-auto" method="POST" action="resgister.php">
+<form class="max-w-md mx-auto" method="POST" action="resgister.php<?php if(isset($_GET['id'])){
+          echo "id=update"; }?>">
     <div class="relative z-0 w-full mb-5 group">
         <input type="text" name="nom" value="<?php if(isset($_GET['id'])){
           echo $nom; } ?>" id="nom" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
